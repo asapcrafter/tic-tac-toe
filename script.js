7ca0c43520243482a3e0c5b96ec584676e3e7c52
Steve <asapcrafter@gmail.com>
2021-08-02 23:02:09 -0700

    added grids on html

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-var gameboard = {
+const Square = (() => {
+    const Square = () => {
+        var status = '';
+        
+        const checkStatus = () => status;
+        const playX = () => status = "x";
+        const playO = () => status = "o";
+        const playNone = () => status = '';
+        return {checkStatus, playX, playO, playNone, checkStatus}
+    };
+    var One = Square();
+    var Two = Square();
+    var Three = Square();
+    var Four = Square();
+    var Five = Square();
+    var Six = Square();
+    var Seven = Square();
+    var Eight = Square();
+    var Nine = Square();
+    return{One, Two, Three, Four, Five, Six, Seven, Eight, Nine};
+})();
+
+var s4 = document.querySelector("#square-4")
+s4.innerHTML = `${Square.One.checkStatus()}`
+
+
+
+
+
+
+
+
+var Player = {
 
 };
 
-var player = {
-
-};
+/*
+Gameboard object has 9 squares
+each square has 3 status: x/o/null
+    square status is connect to DOM
+Gb object has win/lose property
+win/lose property determined by correct lines
+correct lines can be defined by either player X or player O
+squareSix.status(x) => status = x
+*/
